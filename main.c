9cf7931df46113d62b9897d85f682f9413a2a179
kururin-DOT <sofiayu2007@outlook.com>
2025-11-07 15:54:55 -0300

    teste (raylib parou de funcionar)

--- main.c
+++ main.c
@@ -103,6 +103,22 @@ void desenho_tiro(struct tiro **lista,Texture2D piu){
 
 }
 
+typedef enum {
+    MENU = 0,
+    GAMEPLAY = 1,
+    GAMEOVER = 2
+} GameScreen;
+
+void LimparTiros(struct tiro **lista){
+    struct tiro *aux = *lista;
+    while(aux != NULL){
+        struct tiro *temp = aux;
+        aux = aux->next;
+        free(temp);
+    }
+    *lista = NULL;
+}
+
 int main(){
     InitWindow(100, 100,"temp");
     int monitor=GetCurrentMonitor();
@@ -116,7 +132,7 @@ int main(){
 
     int x=largura/2;
     int y=altura-50;
-    int a=0,pontos=0;
+    int pontos=0;
     struct nave_status status;
     status.vida=4;
     
@@ -136,80 +152,126 @@ int main(){
     struct tiro *n=NULL;
     
     PlayMusicStream(musica);
+
+    GameScreen currentScreen = MENU; 
+
     while(!WindowShouldClose()){
         UpdateMusicStream(musica);
 
-        if(IsKeyDown(KEY_S) && y+3<=altura-50){
-            y+=3;
+        if(currentScreen == MENU){
+            if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)){
+                pontos = 0;
+                status.vida = 4;
+                x = largura/2;
+                y = altura-50;
+                posicao_inimigo.x = 400; posicao_inimigo.y = 30;
+                LimparTiros(&n);
+                currentScreen = GAMEPLAY;
+            }
+        } else if(currentScreen == GAMEOVER){
+            if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)){
+                pontos = 0;
+                status.vida = 4;
+                x = largura/2;
+                y = altura-50;
+                posicao_inimigo.x = 400; posicao_inimigo.y = 30;
+                LimparTiros(&n);
+                currentScreen = GAMEPLAY;
+            } else if(IsKeyPressed(KEY_ESCAPE)){
+                currentScreen = MENU;
+            }
+        } else if(currentScreen == GAMEPLAY){
+            if(IsKeyDown(KEY_S) && y+3<=altura-50){
+                y+=3;
+            }
+            if(IsKeyDown(KEY_D) && x+3<=largura-50){
+                x+=3;
+            }
+            if(IsKeyDown(KEY_A) && x-3>=0){
+                x-=3;
+            }
+            if(IsKeyDown(KEY_W) && y-3>=0){
+                y-=3;
+            }
+            if(IsKeyPressed(KEY_E)){
+                Vector2 pos={x,y};
+                Atirar(&n, pos);
+                PlaySound(tiro); 
+            }
+            if(IsKeyPressed(KEY_ESCAPE)){
+                currentScreen = MENU;
+                LimparTiros(&n);
+            }
         }
-        if(IsKeyDown(KEY_D) && x+3<=largura-50){
-            x+=3;
-        }
-        if(IsKeyDown(KEY_A) && x-3>=0){
-            x-=3;
-        }
-        if(IsKeyDown(KEY_W) && y-3>=0){
-            y-=3;
-        }
-        if(IsKeyPressed(KEY_E)){
-            Vector2 pos={x,y};
-            Atirar(&n, pos);
-            PlaySound(tiro); 
-            
-        }
+
         Rectangle rectNaves_Jogador = { x, y, nave.width, nave.height };
         Rectangle rectNaves_de_Bonus = { posicao_inimigo.x, posicao_inimigo.y, Nave_de_bonus.width, Nave_de_bonus.height };
 
-        if(CheckCollisionRecs(rectNaves_de_Bonus,rectNaves_Jogador)){
-            status.vida-=1;
-            posicao_inimigo.y = 0;
+        if(currentScreen == GAMEPLAY){
+            if(CheckCollisionRecs(rectNaves_de_Bonus,rectNaves_Jogador)){
+                status.vida-=1;
+                posicao_inimigo.y = 0;
+            }
         }
-
-
 
         BeginDrawing();
         ClearBackground(WHITE);
         DrawTexture(espaco, 0, 0, WHITE);
         DrawText(TextFormat("SCORE: %i", pontos), 0, 0, 40, MAROON);
 
-        switch (status.vida) {
-            case 4:
-                DrawTexture(nave,x,y,WHITE);
-                break;
-            case 3:
-                DrawTexture(nave_levemente_danificada,x,y,WHITE);
-                break;
-            
-            case 2:
-                DrawTexture(nave_danificada,x,y,WHITE);
+        switch(currentScreen){
+            case MENU:
+                DrawText("SPACE INVADERS", largura/2 - 200, altura/2 - 60, 50, BLACK);
+                DrawText("Pressione ENTER ou SPACE para iniciar", largura/2 - 220, altura/2, 20, DARKGRAY);
+                DrawText("Use WASD para mover, E para atirar, ESC para voltar", largura/2 - 300, altura/2 + 30, 18, DARKGRAY);
                 break;
 
-            case 1:
-                DrawTexture(nave_ultima_vida,x,y,WHITE);
+            case GAMEPLAY:
+                switch (status.vida) {
+                    case 4:
+                        DrawTexture(nave,x,y,WHITE);
+                        break;
+                    case 3:
+                        DrawTexture(nave_levemente_danificada,x,y,WHITE);
+                        break;
+                    case 2:
+                        DrawTexture(nave_danificada,x,y,WHITE);
+                        break;
+                    case 1:
+                        DrawTexture(nave_ultima_vida,x,y,WHITE);
+                        break;
+                    default:
+                        currentScreen = GAMEOVER;
+                        break;
+                }
+
+                DrawTexture(Nave_de_bonus,posicao_inimigo.x,posicao_inimigo.y,WHITE);
+                desenho_tiro(&n,  piu);
                 break;
 
-            default:
-                status.vida=4;
-                pontos=0;
-
+            case GAMEOVER:
+                DrawText("GAME OVER", largura/2 - 150, altura/2 - 40, 60, RED);
+                DrawText(TextFormat("SCORE: %i", pontos), largura/2 - 80, altura/2 + 30, 30, MAROON);
+                DrawText("Pressione ENTER para reiniciar ou ESC para voltar ao menu", largura/2 - 280, altura/2 + 80, 20, DARKGRAY);
+                break;
         }
-        DrawTexture(Nave_de_bonus,posicao_inimigo.x,posicao_inimigo.y,WHITE);
-        
-        desenho_tiro(&n,  piu);
-        
+
         EndDrawing();
-        if(direct_inimigo){
-            posicao_inimigo.x += GetFrameTime() * 200;
-        } else{
-            posicao_inimigo.x -= GetFrameTime() * 200;
+
+        if(currentScreen == GAMEPLAY){
+            if(direct_inimigo){
+                posicao_inimigo.x += GetFrameTime() * 200;
+            } else{
+                posicao_inimigo.x -= GetFrameTime() * 200;
+            }
+            if (posicao_inimigo.x < 0){
+                direct_inimigo=1;
+            } else if(posicao_inimigo.x>largura-100){
+                direct_inimigo=0;
+            }
+            Avancar_tiro(&n);
+            checar_tiro(&n, rectNaves_de_Bonus, &pontos,&posicao_inimigo.x);
         }
-        if (posicao_inimigo.x < 0){
-            direct_inimigo=1;
-        } else if(posicao_inimigo.x>largura-100){
-            direct_inimigo=0;
-        }
-        Avancar_tiro(&n);
-        checar_tiro(&n, rectNaves_de_Bonus, &pontos,&posicao_inimigo.x);
     }
     StopMusicStream(musica);
     UnloadMusicStream(musica);
